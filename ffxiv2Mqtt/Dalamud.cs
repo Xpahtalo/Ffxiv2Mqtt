fb0d128577fe28477bb3589a186b7807b0621ab0
Xpahtalo <106037495+Xpahtalo@users.noreply.github.com>
2022-06-25 19:10:00 -0700

    Remove unused services

--- ffxiv2Mqtt/Dalamud.cs
+++ ffxiv2Mqtt/Dalamud.cs
@@ -18,17 +18,17 @@ public class Dalamud
     public static void Initialize(DalamudPluginInterface pluginInterface)
         => pluginInterface.Create<Dalamud>();
 
-    [PluginService][RequiredVersion("1.0")] public static DalamudPluginInterface PluginInterface { get; private set; } = null!;
-    [PluginService][RequiredVersion("1.0")] public static CommandManager Commands { get; private set; } = null!;
-    [PluginService][RequiredVersion("1.0")] public static SigScanner SigScanner { get; private set; } = null!;
+    //[PluginService][RequiredVersion("1.0")] public static DalamudPluginInterface PluginInterface { get; private set; } = null!;
+    //[PluginService][RequiredVersion("1.0")] public static CommandManager Commands { get; private set; } = null!;
+    //[PluginService][RequiredVersion("1.0")] public static SigScanner SigScanner { get; private set; } = null!;
     [PluginService][RequiredVersion("1.0")] public static DataManager GameData { get; private set; } = null!;
     [PluginService][RequiredVersion("1.0")] public static ClientState ClientState { get; private set; } = null!;
-    [PluginService][RequiredVersion("1.0")] public static ChatGui Chat { get; private set; } = null!;
-    [PluginService][RequiredVersion("1.0")] public static Framework Framework { get; private set; } = null!;
+    //[PluginService][RequiredVersion("1.0")] public static ChatGui Chat { get; private set; } = null!;
+    //[PluginService][RequiredVersion("1.0")] public static Framework Framework { get; private set; } = null!;
     [PluginService][RequiredVersion("1.0")] public static Condition Conditions { get; private set; } = null!;
-    [PluginService][RequiredVersion("1.0")] public static TargetManager Targets { get; private set; } = null!;
-    [PluginService][RequiredVersion("1.0")] public static ObjectTable Objects { get; private set; } = null!;
-    [PluginService][RequiredVersion("1.0")] public static TitleScreenMenu TitleScreenMenu { get; private set; } = null!;
+    //[PluginService][RequiredVersion("1.0")] public static TargetManager Targets { get; private set; } = null!;
+    //[PluginService][RequiredVersion("1.0")] public static ObjectTable Objects { get; private set; } = null!;
+    //[PluginService][RequiredVersion("1.0")] public static TitleScreenMenu TitleScreenMenu { get; private set; } = null!;
     [PluginService][RequiredVersion("1.0")] public static GameGui GameGui { get; private set; } = null!;
     [PluginService][RequiredVersion("1.0")] public static JobGauges JobGauges { get; private set; } = null!;
 }
