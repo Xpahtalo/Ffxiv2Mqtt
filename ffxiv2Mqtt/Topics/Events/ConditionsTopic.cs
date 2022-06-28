fa37210f58208b71347ee596560d6f0b02a6b542
Xpahtalo <106037495+Xpahtalo@users.noreply.github.com>
2022-06-27 18:07:32 -0700

    Add ID to conditions and send the message as JSON
    #2

--- ffxiv2Mqtt/Topics/Events/ConditionsTopic.cs
+++ ffxiv2Mqtt/Topics/Events/ConditionsTopic.cs
@@ -1,10 +1,13 @@
-﻿using Dalamud.Game.ClientState.Conditions;
+﻿using Newtonsoft.Json;
+using Dalamud.Game.ClientState.Conditions;
 using Ffxiv2Mqtt.TopicTracker.Interfaces;
 
 namespace Ffxiv2Mqtt.TopicTracker.Events
 {
     internal class ConditionsTopic : Topic, ICleanable
     {
+
+        
         internal ConditionsTopic(MqttManager mqttManager) : base(mqttManager)
         {
             topic = "Player/Conditions";
@@ -14,7 +17,7 @@ namespace Ffxiv2Mqtt.TopicTracker.Events
 
         private void ConditionChange(ConditionFlag flag, bool value)
         {
-            mqttManager.PublishMessage($"{topic}/{flag}", value.ToString());
+            mqttManager.PublishMessage($"{topic}/{flag}", JsonConvert.SerializeObject(new { Id = (int)flag, Active = value }));
         }
 
         public void Cleanup()
