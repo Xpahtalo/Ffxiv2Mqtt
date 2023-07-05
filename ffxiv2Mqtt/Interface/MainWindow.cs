a1e535243a5d25afe7009a90e05ce6e2184e9fb7
Xpahtalo <106037495+Xpahtalo@users.noreply.github.com>
2023-07-05 14:10:08 -0700

    tabs converted

--- ffxiv2Mqtt/Interface/MainWindow.cs
+++ ffxiv2Mqtt/Interface/MainWindow.cs
@@ -38,16 +38,17 @@ internal class MainWindow : Window
     }
 
 #region Status
+
     private void DisplayStatusTab()
     {
-        if (!ImGui.BeginTabItem("Status"))
+        using var statusTab = ImRaii.TabItem("Status");
+        if (!statusTab)
             return;
-
 
         if (configuration.BrokerAddress == string.Empty ||
             configuration.User          == string.Empty ||
             configuration.Password      == string.Empty) {
-            ImGui.TextColored(ImGuiColors.HealerGreen,"Please configure your client settings to connect.");
+            ImGui.TextColored(ImGuiColors.HealerGreen, "Please configure your client settings to connect.");
         } else {
             ImGui.Text("Client Started: "    + mqttManager.IsStarted);
             ImGui.Text("Connection Status: " + mqttManager.IsConnected);
@@ -74,35 +75,39 @@ internal class MainWindow : Window
         }
 #endif
 
-        ImGui.EndTabItem();
     }
-    #endregion
 
-    #region Settings
+#endregion
+
+#region Settings
 
     private void DisplayMqttSettingsTab()
     {
-        if (!ImGui.BeginTabItem("Settings"))
+        using var settingsTab = ImRaii.TabItem("Settings");
+        if (!settingsTab)
             return;
 
         var clientId                                                                = configuration.ClientId;
         if (ImGui.InputText("Client ID", ref clientId, 256)) configuration.ClientId = clientId;
 
-        var includeClientId                                                                                   = configuration.IncludeClientId;
-        if (ImGui.Checkbox("Include Client ID in topic?", ref includeClientId)) configuration.IncludeClientId = includeClientId;
-        HelpMarker("This is useful if you have multiple computers connected to the same broker, so you can differentiate between them. Otherwise, leave it off.");
+        var includeClientId = configuration.IncludeClientId;
+        if (ImGui.Checkbox("Include Client ID in topic?", ref includeClientId))
+            configuration.IncludeClientId = includeClientId;
+        HelpMarker(
+            "This is useful if you have multiple computers connected to the same broker, so you can differentiate between them. Otherwise, leave it off.");
 
         var user                                                       = configuration.User;
         if (ImGui.InputText("User", ref user, 256)) configuration.User = user;
 
-        var password                                                                                             = configuration.Password;
-        if (ImGui.InputText("Password", ref password, 256, ImGuiInputTextFlags.Password)) configuration.Password = password;
+        var password = configuration.Password;
+        if (ImGui.InputText("Password", ref password, 256, ImGuiInputTextFlags.Password))
+            configuration.Password = password;
         ColoredMarker(new Vector4(1, 0, 0, 1),
                       "(!)",
                       "Password is stored in plaintext. It is not secure, so please use a unique password for your user.");
 
 
-        var brokerAddress                                                                           = configuration.BrokerAddress;
+        var brokerAddress = configuration.BrokerAddress;
         if (ImGui.InputText("Broker Address", ref brokerAddress, 2000)) configuration.BrokerAddress = brokerAddress;
 
         var brokerPort                                                              = configuration.BrokerPort;
@@ -115,28 +120,29 @@ internal class MainWindow : Window
         if (includeClientId) fullTopic += "/" + clientId;
         ImGui.Text($"All topics will be preceded by: {fullTopic}/");
 
-        var connectAtStartup                                                                            = configuration.ConnectAtStartup;
-        if (ImGui.Checkbox("Connect at startup?", ref connectAtStartup)) configuration.ConnectAtStartup = connectAtStartup;
+        var connectAtStartup = configuration.ConnectAtStartup;
+        if (ImGui.Checkbox("Connect at startup?", ref connectAtStartup))
+            configuration.ConnectAtStartup = connectAtStartup;
 
         var interval                                                              = configuration.Interval;
         if (ImGui.InputInt("Sync Interval", ref interval)) configuration.Interval = interval;
-        HelpMarker("This is used to send messages multiple times as timers tick. 1000 is one second. Set to -1 to disable.");
+        HelpMarker(
+            "This is used to send messages multiple times as timers tick. 1000 is one second. Set to -1 to disable.");
 
         if (ImGui.Button("Save")) {
             topicManager.Configure(configuration);
             configuration.Save();
         }
-
-        ImGui.EndTabItem();
     }
 
-    #endregion
+#endregion
 
-    #region Subscriptions
+#region Subscriptions
 
     private void DisplaySubscriptionSettingsTab()
     {
-        if (!ImGui.BeginTabItem("Subscriptions"))
+        using var subscriptionsBar = ImRaii.TabItem("Subscriptions");
+        if (!subscriptionsBar)
             return;
 
         DrawAddSubscriptionButton();
@@ -146,8 +152,6 @@ internal class MainWindow : Window
             configuration.Save();
             mqttManager.ConfigureSubscribedTopics();
         }
-
-        ImGui.EndTabItem();
     }
 
     private void DrawAddSubscriptionButton()
@@ -207,9 +211,9 @@ internal class MainWindow : Window
         }
     }
 
-    #endregion
+#endregion
 
-    #region Helpers
+#region Helpers
 
     private static void HelpMarker(string text)
     {
@@ -231,6 +235,6 @@ internal class MainWindow : Window
         ImGui.EndTooltip();
     }
 
-    #endregion
+#endregion
 
 }
